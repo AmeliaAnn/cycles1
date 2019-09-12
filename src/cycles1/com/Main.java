a01ab3ac4dad754479cc98f6a308121afba3d8dd
root <root@localhost.localdomain>
2019-09-12 19:22:55 +0300

    Initial commit

---
+++ src/cycles1/com/Main.java
@@ -0,0 +1,14 @@
+package cycles1.com;
+
+ class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i<=50; i++)
+        {
+            System.out.print(i + "\n");
+        }
+
+
+    }
+}
